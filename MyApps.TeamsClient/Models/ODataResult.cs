60ae245af72187c81b6af3632b165b95cfc41594
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2023-06-09 17:30:40 +0200

    Add project files.

---
+++ MyApps.TeamsClient/Models/ODataResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MyApps.TeamsClient.Models
+{
+    public class ODataResult<TValue>
+    {
+        [JsonPropertyName("@odata.context")]
+        public string Context { get; set; }
+        public TValue Value { get; set; }
+    }
+}
---
+++ MyApps.TeamsClient/Models/Tile.cs
@@ -0,0 +1,14 @@
+﻿namespace MyApps.TeamsClient.Models
+{
+    public class Tile
+    {
+        public string Id { get; set; }
+        public string Provider { get; set; }
+        public string ProviderId { get; set; }
+        public string DisplayName { get; set; }
+        public string LogoUrl { get; set; }
+        public string FastLaunchUrl { get; set; }
+        public string PersistentLaunchUrl  { get; set; }
+        public string AssignmentType { get; set; }
+    }
+}
