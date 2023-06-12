aeac435b66f8bcdd931a4890a78d9104dbc0a409
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2023-06-12 16:50:04 +0200

    Added Blog posts

---
+++ MyApps.TeamsClient/Models/JsonFeed.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Text.Json.Serialization;
+
+namespace MyApps.TeamsClient.Models
+{
+    public class JsonFeed
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [JsonPropertyName("items")]
+        public IEnumerable<JsonPost> Posts { get; set; }
+    }
+}
---
+++ MyApps.TeamsClient/Models/JsonPost.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Text.Json.Serialization;
+
+namespace MyApps.TeamsClient.Models
+{
+    public class JsonPost
+    {
+        public string Title { get; set; }
+        [JsonPropertyName("date_published")]
+        public DateTimeOffset? DatePublished { get; set; }
+        public string Url { get; set; }
+        public string Summary { get; set; }
+        public IEnumerable<string>? Tags { get; set; }
+
+        public string FullUrl => $"https://svrooij.io{Url}";
+
+    }
+}
